ac75cb77b9619522549b0f1906e436b68405b07b
hiukim <hiukim@gmail.com>
2015-10-23 17:04:37 +0800

    telegram api integration

---
+++ server/main.es6.js
@@ -0,0 +1,28 @@
+let setupIndexes = function() {
+  TelegramService.Messages._ensureIndex({
+    chat_id: 1,
+    id: 1
+  });
+
+  TelegramService.Chats._ensureIndex({
+    id: 1
+  });
+};
+
+let setupBots = function() {
+  TelegramService.Bots.find().observe({
+    added: function(bot) {
+      bot.setup();
+    },
+
+    removed: function(bot) {
+      // TODO handle
+    }
+  });
+}
+
+Meteor.startup(function() {
+  setupIndexes();
+
+  setupBots();
+});
